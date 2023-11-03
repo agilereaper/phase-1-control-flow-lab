ff5cff693d93d6ea771f290616ad871fc60da197
dillon zett <dillonmzett@gmail.com>
2023-11-03 08:30:04 -0500

    working on it

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(greeting){
+  if (greeting <= 400) {
+    return 'This one is on me!';
+
+  } else if (greeting >= 400 && greeting <= 2000) {
+    return 'That will be twenty bucks.';
+
+  } else if (greeting >= 2000 && greeting <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+
+  } else (greeting > 2500) 
+    return 'No can do.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city = NYC) {
+    return "Ok, sounds good.";
+  } else if (city = Pittsburgh)
+  return 'No go.'
 }
-
 function switchOnCharmFromTip(){
   // Write your code here!
 }
